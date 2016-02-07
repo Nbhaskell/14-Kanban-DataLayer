dd34aa1c2a42020c2b60e82a2a63495388497a7b
Nathan Haskell <Nathan Haskell>
2016-02-06 21:00:42 -0800

    update

--- KanbanDataLayer/KanbanDataLayer/Program.cs
+++ KanbanDataLayer/KanbanDataLayer/Program.cs
@@ -71,7 +71,7 @@ namespace KanbanDataLayer
                         continue;
                     }
                 }
-            } 
+            }
             Console.ReadKey();
         }
     }
